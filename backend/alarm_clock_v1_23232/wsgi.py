f2ea80846e2e4aa15dd3c9ba1a841facbf99e823
Crowdbotics <team@crowdbotics.com>
2020-12-07 11:34:01 +0000

    Update models

--- backend/alarm_clock_v1_23232/wsgi.py
+++ backend/alarm_clock_v1_23232/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alarm_clock_v1_23232.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alarm_clock_v1_23232.settings")
 
 application = get_wsgi_application()
